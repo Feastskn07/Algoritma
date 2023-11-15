45a157f5a345ad8df40857460d147db534936ae7
İbrahim Taşkın <77129766+Feastskn07@users.noreply.github.com>
2023-11-15 14:28:11 +0300

    Add files via upload

--- Ödev-3/ucuncusoru.c
+++ Ödev-3/ucuncusoru.c
@@ -6,10 +6,10 @@ int main()
     printf("Lutfen a, b, c sayilarini giriniz: ");
     scanf("%d, %d, %d", &a, &b, &c);
 
-    if (a>b , a>c)
+    if (a>b && a>c)
     {
         printf("%d",a);
-    }else if (b>a , b>c)
+    }else if (b>a && b>c)
     {
         printf("%d",b);
     }else
